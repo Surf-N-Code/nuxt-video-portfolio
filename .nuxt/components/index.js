c509a394d42fd556c2897f11c9d0f1fa7d38ec5f
Norman <norman@yazio.com>
2021-04-29 21:14:14 +0200

    Remove .nuxt from git

--- .nuxt/components/index.js
+++
@@ -1,5 +0,0 @@
-import { wrapFunctional } from './utils'
-
-export { default as Profile } from '../../components/Profile.vue'
-
-export const LazyProfile = import('../../components/Profile.vue' /* webpackChunkName: "components/profile" */).then(c => wrapFunctional(c.default || c))
